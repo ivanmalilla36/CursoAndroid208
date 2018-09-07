8bb92af4a75bc9a9ab926387d566e2d5abac47ef
ivanmalilla36 <ivanmalilla36@gmail.com>
2018-09-06 18:21:03 -0600

    se termino la tarea 5

--- tarea5/app/src/main/java/com/example/egghunt3r/tarea5/MainActivity.kt
+++ tarea5/app/src/main/java/com/example/egghunt3r/tarea5/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.egghunt3r.tarea5
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -14,7 +16,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var boton = findViewById<Button>(R.id.calcular)
+        var boton = findViewById<Button>(R.id.boton)
         var ps = findViewById<EditText>(R.id.peso)
         var al = findViewById<EditText>(R.id.altura)
         var resul = findViewById<TextView>(R.id.resultado)
@@ -24,22 +26,26 @@ class MainActivity : AppCompatActivity() {
             var peso = ps.text.toString()
             var altura = al.text.toString()
 
-            Log.d("peso",peso)
-            Log.d("altura",altura)
+//            Log.d("peso",peso.toString())
+//            Log.d("altura",altura.toString())
 
-            if (peso == null || altura == null){
-                Toast.makeText(this, "Favor de ingresar los valores de altura y peso",Toast.LENGTH_SHORT).show()
+            if (peso.equals("") || altura.equals("")){
+                Toast.makeText(this, "Debe ingresar todos los valores",Toast.LENGTH_SHORT).show()
             }
+            else{
+                var imc = peso.toDouble() / (altura.toDouble() * altura.toDouble())
+                resul.setText("el imc es de {$imc}")
 
-            var imc = peso.toDouble() / (altura.toDouble() * altura.toDouble())
-            resul.setText("el imc es de ($imc)")
 
-            var status = imc
-            if (status > 18 || status < 24.9){
-                stat.setText("El paciente se encuentra en el peso adecuado")
-            }
-            else {
-                stat.setText("El paciente NO se encuentra en el peso adecuado")
+                if (imc >= 18 || imc <= 24.9){
+                    stat.setText("El paciente se encuentra en el peso adecuado")
+                }
+                else {
+                    stat.setText("El paciente NO se encuentra en el peso adecuado")
+                }
+
+                val inputManager: InputMethodManager =getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputManager.hideSoftInputFromWindow(currentFocus.windowToken, InputMethodManager.SHOW_FORCED)
             }
 
         })
