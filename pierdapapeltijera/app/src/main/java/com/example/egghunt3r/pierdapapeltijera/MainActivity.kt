555b72595a68a3b34a4a7d5471a0dffde51abec8
ivanmalilla36 <ivanmalilla36@gmail.com>
2018-09-06 19:46:36 -0600

    se agrego sensor de movimiento en el proyecto

--- pierdapapeltijera/app/src/main/java/com/example/egghunt3r/pierdapapeltijera/MainActivity.kt
+++ pierdapapeltijera/app/src/main/java/com/example/egghunt3r/pierdapapeltijera/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.egghunt3r.pierdapapeltijera
 
+import android.content.Context
+import android.hardware.SensorManager
 import android.media.Image
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -8,12 +10,29 @@ import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import com.goodcodeforfun.seismik.ShakeDetector
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ShakeDetector.Listener {
+    override fun hearShake() {
+        var imagen = findViewById<ImageView>(R.id.imagen)
+        val random1 = (0..2).shuffled().last()
+//            Toast.makeText(this,"tu numero es ${random1}",Toast.LENGTH_SHORT).show()
+
+        when (random1) {
+            0 -> imagen.setImageResource(R.drawable.pierda)
+            1 -> imagen.setImageResource(R.drawable.papel)
+            2 -> imagen.setImageResource(R.drawable.tijera)
+        }
+        Toast.makeText(this,"shake",Toast.LENGTH_SHORT).show()
+    }
+
+    private lateinit var shakeDetector: ShakeDetector
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        shakeDetector = ShakeDetector(this)
 
         var boton = findViewById<Button>(R.id.btn)
         var imagen = findViewById<ImageView>(R.id.imagen)
@@ -29,6 +48,17 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
-
     }
+
+    override fun onStart() {
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        shakeDetector.start(sensorManager)
+        super.onStart()
+    }
+
+    override fun onStop() {
+        shakeDetector.stop()
+        super.onStop()
+    }
+
 }
