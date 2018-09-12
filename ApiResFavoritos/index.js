95a0037573f33f8bb0391593178b2c6d767e03a7
ivanmalilla36 <ivanmalilla36@gmail.com>
2018-09-11 18:48:53 -0600

    se agrego lo de la clase de internet

--- ApiResFavoritos/index.js
+++ ApiResFavoritos/index.js
@@ -1,20 +1,9 @@
 'use strict'
 
-const express = require('express');
-const bodyParser = require('body-parser')
-const app = express();
+const app = require('./app');
 const port = 3678;
-
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
-
-app.get('/', function (req, res) {
-	console.log("ya hizo la peticion")
-	res.send("Hola Mundo")
-})
 
 app.listen(port, () => {
 	console.log(`API RES FAVORITOS funcionando en en http://localhost:${port}`);
 });
 
-console.log("hola!");
