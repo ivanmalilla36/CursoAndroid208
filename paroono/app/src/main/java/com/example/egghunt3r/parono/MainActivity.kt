a471c09d90535e82138c76089ebc5bc736e16eb3
ivanmalilla36 <ivanmalilla36@gmail.com>
2018-09-11 18:49:29 -0600

    aplicacion de par o no

---
+++ paroono/app/src/main/java/com/example/egghunt3r/parono/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.egghunt3r.parono
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var boton = findViewById<Button>(R.id.button)
+        var num = findViewById<EditText>(R.id.numero)
+        var resultado = findViewById<TextView>(R.id.res)
+
+
+        boton.setOnClickListener(View.OnClickListener {
+
+            val numero = num.text.toString()
+
+            if (numero.toInt() %2== 0){
+                resultado.setText("el numero es par ${numero}")
+            }
+            else {
+                resultado.setText("El numero no es par  ${numero}")
+            }
+        })
+    }
+
+
+
+}
